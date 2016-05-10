a9b08326a58898206665b29013e42f20ba328d4f
emeyer161 <meyer.ej@gmail.com>
2016-05-10 18:28:44 +0000

    Test for 5.5

--- application/app/Repositories/Repository.php
+++ application/app/Repositories/Repository.php
@@ -6,37 +6,41 @@ abstract class Repository
 {
     public function create(array $data)
     {
-        $model = $this->model;
-        dd($this);
-        return $this->model::create( $data );
+        $model = new $this->model;
+        return $model::create( $data );
     }
 
     public function getAll()
     {
-        return $this->model::all();
+        $model = new $this->model;
+        return $model::all();
 
     }
 
     public function getById($id)
     {
-        return $this->model::find( $id );
+        $model = new $this->model;
+        return $model::find( $id );
     }
 
     public function update(array $data, $id)
     {
-        $this->model::find($id)->fill( $data )->save();
-        return $this->model::find( $id );
+        $model = new $this->model;
+        $model::find($id)->fill( $data )->save();
+        return $model::find( $id );
     }
 
     public function delete($model)
     {
+        $model = new $this->model;
         $model->delete();
         return response()->json(true);
     }
 
     public function destroy($id)
     {
-        $this->model::destroy( $id );
+        $model = new $this->model;
+        $model::destroy( $id );
         return response()->json(true);
     }
 }
