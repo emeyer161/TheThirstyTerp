6b88d3f1e751954c3fb32c0199134b133fc5dba6
emeyer161 <meyer.ej@gmail.com>
2016-05-11 02:21:21 -0400

    UI Updates

--- application/resources/views/resource/comments/create.blade.php
+++ application/resources/views/resource/comments/create.blade.php
@@ -1,12 +1,12 @@
 <form action="/comments" method="POST">
-	<fieldset class="form-group">
-	    <label for="body">New Comment</label>
-	    <textarea class="form-control" name="body" id="body" rows="2"></textarea>
-	</fieldset>
-
     <input type="hidden" name="user_id" value="{{Auth::user()['id']}}">
 	<input type="hidden" name="post_id" value="{{$post['id']}}">
 	<input type="hidden" name="_token" value="{{ csrf_token() }}">
 
-	<button type="submit" class="btn btn-primary btn-large">Submit</button>
+	<div class="input-group">
+	    <input class="form-control" name="body" id="body" placeholder="Add a comment..."></textarea>
+	    <span class="input-group-btn">
+	        <button type="submit" class="btn btn-success btn-large">Submit</button>
+		</span>
+	</div>
 </form>
