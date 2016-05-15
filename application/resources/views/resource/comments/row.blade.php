753d61146270ba0062544e0a6b09156beff33e05
emeyer161 <meyer.ej@gmail.com>
2016-05-15 13:40:16 -0400

    comment deleting

--- application/resources/views/resource/comments/row.blade.php
+++ application/resources/views/resource/comments/row.blade.php
@@ -1,10 +1,12 @@
 <div class="blog-post-meta">
+    @can('delete-comment', $comment)
+    	<div style="float:right">
+			@include('resource.delete-button', array('url' => action('Client\CommentsController@destroy', ['id' => $comment->id]) ))
+    	</div>
+    @endcan
 	<h4>{{ $comment->user->user_name.' on '.date('F d, g:ia', strtotime($comment->created_at)) }}</h4>
 
 	<p>{{ $comment['body'] }}</p>
     
-    @can('delete-comment', $comment)
-		@include('resource.delete-button', array('url' => action('Client\CommentsController@destroy', ['id' => $comment->id]) ))
-    @endcan
 </div>
 <hr>
